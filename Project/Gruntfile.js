4d2db9b892af95d08ae69f55eb4a5099b9360491
Monteiro <jm1046972@bose.com>
2019-11-26 17:48:28 +0000

    Project setup and autoprefixer

--- Project/Gruntfile.js
+++ Project/Gruntfile.js
@@ -1,11 +1,27 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON("package.json"),
+        // autoprefixer
+        autoprefixer: {
+            options: {
+                browsers: [
+                    "> 1% in PT",
+                    "last 2 versions",
+                    "firefox > 3",
+                    "ie9"
+                ]
+            },
+            // dist is just a var, any name
+            dist: {
+                src: "css/app.css",
+                dest: "css/app-prefixed.css"
+            }
+        }
     });
+
+    grunt.loadNpmTasks("grunt-autoprefixer");
+
+    grunt.registerTask("default", ["grunt-autoprefixer"]);
+    // grunt.registerTask("myOwnTask", "Do something", ["grunt-autoprefixer"]);
 };
